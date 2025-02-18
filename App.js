603a51a2ff924783b89ccbfe1f8f44c09548952a
meranamkarthikeyavarma <seetarama07@gmail.com>
2025-02-18 08:29:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MedicalDataExtraction from "./components/MedicalDataExtraction";
+
+function App() {
+    return (
+        <div className="App">
+            <MedicalDataExtraction />
+        </div>
+    );
+}
+
+export default App;
